283a9abe6f1f29bb2b8b4b2dd50b23fb73a7ea12
Samson <samsono@gmail.com>
2022-10-11 14:05:19 -0400

    Add constants file

---
+++ src/Constants.js
@@ -0,0 +1,30 @@
+export const ACTION = {
+    TYPE:{
+        NUMBER:'NUMBER',
+        SYMBOL:'SYMBOL'
+    },
+    VALUE:{
+        NUMBER_0:'0',
+        NUMBER_1:'1',
+        NUMBER_2:'2',
+        NUMBER_3:'3',
+        NUMBER_4:'4',
+        NUMBER_5:'5',
+        NUMBER_6:'6',
+        NUMBER_7:'7',
+        NUMBER_8:'8',
+        NUMBER_9:'9',
+        NUMBER_DOT:'.',
+        SYMBOL_EQUAL:'=',
+        SYMBOL_PLUS:'+',
+        SYMBOL_MINUS:'-',
+        SYMBOL_DIVIDE:'/',
+        SYMOBL_MULTIPLE:'*'
+    }
+}
+
+export const BUTTON_COLOR = {
+    DARK_BUTTON_LOW:'dark-button-low',
+    DARK_BUTTON_MEDIUM:'dark-button-medium',
+    DARK_BUTTON_HIGH:'dark-button-high'
+}
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import CalculatorDisplayFormula from './Calculator.Display.Formula';
 import CalculatorDisplayResult from './Calculator.Display.Result';
 import CalculatorRowButtons from './Calculator.Row.Buttons';
+import { ACTION } from '../Constants';
 
 const Content = styled.div `
  /* Content */
@@ -27,25 +28,25 @@ const Calculator = ({}) => {
             <CalculatorDisplayFormula/>
             <CalculatorDisplayResult />
             <CalculatorRowButtons buttons={[
-                {title:'Seven',display:'7',buttonColor:'dark-button-low',actionName:'Num7'},
-                {title:'Eight',display:'8',buttonColor:'dark-button-low',actionName:'Num8'},
-                {title:'Nine',display:'9',buttonColor:'dark-button-low',actionName:'Num9'},
-                {title:'Divide',display:'÷',buttonColor:'dark-button-high',actionName:'Divide'}]}/>
+                {display:'7',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_7},
+                {display:'8',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_8},
+                {display:'9',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_9},
+                {display:'÷',buttonColor:'dark-button-high',actionName:ACTION.TYPE.SYMBOL, actionValue:ACTION.VALUE.SYMBOL_DIVIDE}]}/>
             <CalculatorRowButtons buttons={[
-                {title:'Four',display:'4',buttonColor:'dark-button-low',actionName:'Num4'},
-                {title:'Five',display:'5',buttonColor:'dark-button-low',actionName:'Num5'},
-                {title:'Six',display:'6',buttonColor:'dark-button-low',actionName:'Num6'},
-                {title:'Multiple',display:'×',buttonColor:'dark-button-high',actionName:'Multiple'}]}/>
+                {display:'4',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_4},
+                {display:'5',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_5},
+                {display:'6',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_6},
+                {display:'×',buttonColor:'dark-button-high',actionName:ACTION.TYPE.SYMBOL, actionValue:ACTION.VALUE.SYMOBL_MULTIPLE}]}/>
             <CalculatorRowButtons buttons={[
-                {title:'One',display:'1',buttonColor:'dark-button-low',actionName:'Num1'},
-                {title:'Two',display:'2',buttonColor:'dark-button-low',actionName:'Num2'},
-                {title:'Three',display:'3',buttonColor:'dark-button-low',actionName:'Num3'},
-                {title:'Minus',display:'-',buttonColor:'dark-button-high',actionName:'Minus'}]}/>
+                {display:'1',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_1,
+                {display:'2',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_2},
+                {display:'3',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_3},
+                {display:'-',buttonColor:'dark-button-high',actionName:ACTION.TYPE.SYMBOL, actionValue:ACTION.VALUE.SYMBOL_MINUS}]}/>
             <CalculatorRowButtons buttons={[
-                {title:'Zero',display:'0',buttonColor:'dark-button-low',actionName:'Num1'},
-                {title:'Decimal',display:'.',buttonColor:'dark-button-low',actionName:'Num2'},
-                {title:'Plus',display:'+',buttonColor:'dark-button-high',actionName:'Plus'},
-                {title:'Equal',display:'=',buttonColor:'dark-button-high',actionName:'Equal'}]}/>
+                {display:'0',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_0},
+                {display:'.',buttonColor:'dark-button-low',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.NUMBER_DOT},
+                {display:'+',buttonColor:'dark-button-high',actionName:ACTION.TYPE.NUMBER, actionValue:ACTION.VALUE.SYMBOL_PLUS},
+                {display:'=',buttonColor:'dark-button-high',actionName:ACTION.TYPE.SYMBOL, actionValue:ACTION.VALUE.SYMBOL_EQUAL}]}/>
         </Content>  
     );
   };
